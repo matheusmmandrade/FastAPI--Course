42ee0b6ec189f1d5bd33039cc6dbdaebe626f57b
Matheus Matos <matheusmmandrade@gmail.com>
2025-06-15 11:38:43 -0300

    Add: HTTP exceptions

--- app/main.py
+++ app/main.py
@@ -1,57 +1,35 @@
-from fastapi import FastAPI
-from scalar_fastapi import get_scalar_api_reference
 from typing import Any
+
+from fastapi import FastAPI, HTTPException, status
+from scalar_fastapi import get_scalar_api_reference
+
+
 app = FastAPI()
 
-
 shipments = {
-    10291: {
-        "weight": 1,
-        "content": "wooden table",
-        "status": "in transit"
-    },
-    1441: {
-        "weight": 10.5,
-        "content": "iron table",
-        "status": "in transit"
-    },
-    1: {
-        "weight": 13,
-        "content": "diamond table",
-        "status": "in transit"
-    },
-    1191: {
-        "weight": 20.6,
-        "content": "gold table",
-        "status": "in transit"
-    },
-    10231: {
-        "weight": 9,
-        "content": "ruby table",
-        "status": "in transit"
-    },
-    101: {
-        "weight": 2,
-        "content": "copper table",
-        "status": "in transit"
-    }
+    12701: {"weight": 0.6, "content": "glassware", "status": "placed"},
+    12702: {"weight": 2.3, "content": "books", "status": "shipped"},
+    12703: {"weight": 1.1, "content": "electronics", "status": "delivered"},
+    12704: {"weight": 3.5, "content": "furniture", "status": "in transit"},
+    12705: {"weight": 0.9, "content": "clothing", "status": "returned"},
+    12706: {"weight": 4.0, "content": "appliances", "status": "processing"},
+    12707: {"weight": 1.8, "content": "toys", "status": "placed"},
 }
 
 
-@app.get("/shipment/latest")
-def get_latest_shipment():
-    id = max(shipments.keys())
-    return shipments[id]
-
-
-@app.get("/shipment/{id}")
+@app.get("/shipment")
 def get_shipment(id: int) -> dict[str, Any]:
-
+    # Check for shipment with given id
     if id not in shipments:
-        return {"detail": "Given id doesnt exist"}
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Given id doesn't exist!"
+        )
+
     return shipments[id]
 
 
+# Scalar API Documentation
 @app.get("/scalar", include_in_schema=False)
 def get_scalar_docs():
     return get_scalar_api_reference(
