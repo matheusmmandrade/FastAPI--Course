c056150ff10987be06a0a48a25d94dab5adf658f
Matheus Matos <matheusmmandrade@gmail.com>
2025-06-15 09:42:34 -0300

    Add: endpoint GET /scalar - Aula 2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
 
@@ -10,3 +10,11 @@ def get_shipment():
         "content": "wooden table",
         "status": "in transit"
     }
+
+
+@app.get("/scalar", include_in_schema=False)
+def get_scalar_docs():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="Scalar API",
+    )
